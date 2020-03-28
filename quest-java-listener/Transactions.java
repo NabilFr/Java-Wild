276dc328eba97b21632023dbb2c543c0d42abb68
Nabil Benssadek <benweb31@gmail.com>
2020-03-28 13:12:45 +0100

    ras

---
+++ quest-java-listener/Transactions.java
@@ -0,0 +1,21 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Transactions implements TransactionListener{
+
+    @Override
+    public void onComplete(Bank account) {
+
+    }
+
+    public static void withdraw(Bank account, int value, TransactionListener event) {
+        int time = (int) (Math.random() * ((1000 - 500) + 1)) + 500;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                account.setTotal(account.getTotal() - value);
+                event.onComplete(account);
+            }
+        }, time);
+    }
+}
---
+++ quest_java_exceptions/src/main/OddSum.java
@@ -0,0 +1,60 @@
+import java.security.spec.ECField;
+import java.util.ArrayList;
+import java.util.IllegalFormatConversionException;
+
+public class OddSum {
+
+    public static void main(String[] args) {
+        run(args, false);
+    }
+
+    public static int run(String[] args, boolean test) {
+
+        ArrayList<Integer> listInt = new ArrayList<>();
+        int res;
+        if (!test) {
+            System.out.println("Elements' list :");
+            for (String v : args)
+                System.out.print(" " + v);
+            System.out.println();
+        }
+
+
+            for (int i = 0; i < args.length; i++) {
+                try {
+                listInt.add(Integer.parseInt(args[i]));
+                }
+                catch (NumberFormatException e) {
+                    return -1;
+                }
+            }
+
+
+
+        res = sum(listInt);
+        if (test) {
+            return res;
+        }
+
+        System.out.println("Sum of odd elements : " + res);
+        System.out.println("END!");
+
+        return 0;
+    }
+
+    public static int sum(ArrayList<Integer> listInt) {
+
+        int result = 0;
+        for (int value : listInt) {
+            try {
+                if (value % 2 != 0) {
+                    result += value;
+                }
+            }
+            catch (Exception e) {
+                System.out.println("No even numbers");
+            }
+        }
+        return result;
+    }
+}
